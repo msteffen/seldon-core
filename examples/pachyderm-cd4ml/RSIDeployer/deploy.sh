8aaa104de8c6e2a8138632022265271ec070ad1f
Kyle White <kylewhite@gmail.com>
2021-02-10 21:28:22 -0500

    Update to deploy

--- examples/pachyderm-cd4ml/RSIDeployer/deploy.sh
+++ examples/pachyderm-cd4ml/RSIDeployer/deploy.sh
@@ -7,8 +7,4 @@ fi
 
 echo "namespace=${1} model.uri=${2} model.endpoint="${3}" model.auth="${4}" model.ssl="${5}" model.version=${6}"
 
-if helm --namespace=${1} status rsi ; then
-    helm --namespace=${1} upgrade rsi /charts/seldon-deployment --set model.uri=${2} --set model.endpoint=${3} --set model.auth=${4} --set model.ssl=${5} --set model.version=${6}
-else
-    helm --namespace=${1} install rsi /charts/seldon-deployment --set model.uri=${2} --set model.endpoint=${3} --set model.auth=${4} --set model.ssl=${5} --set model.version=${6}
-fi
+helm upgrade rsi /charts/seldon-deployment --namespace=${1} -i --set model.uri=${2} --set model.endpoint=${3} --set model.auth=${4} --set model.ssl=${5} --set model.version=${6}
